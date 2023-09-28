26ab8f24fd070edb4a1ef798d4fbfb2f8ad0e7de
p24jmorris <144147868+JonMorris8@users.noreply.github.com>
2023-09-28 12:13:01 -0400

    FInished assinment

---
+++ lab_3-4.py
@@ -0,0 +1,40 @@
+
+#Create a Python file named lab_3-4.py
+import math
+#Without using Python, determine the value of expressions. Use a comment to record your determined value
+
+print(math.ceil(7.15))
+#8
+#correct
+print(math.floor(19.156))
+#19
+#correct
+print(math.fabs(-42))
+#42
+#correct
+print(math.pow(2, 3))
+#8
+#correct
+print(math.log10(100))
+#10
+#wrong: 2
+print(math.sqrt(9))
+#3
+#correct
+print(math.isqrt(2))
+#1
+#correct
+print(math.factorial(5))
+#120
+#correct
+print(math.gcd(111111111, 12321))
+#333
+#correct
+print(math.sin(0))
+#0
+#correct
+
+#Copy the syntax into your file and check your answers.
+
+
+
